529651dc2b383fd71863692a41d05b3237f92aca
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-11-05 06:21:29 +0200

    footer update (layout)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,28 +2,32 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="footer__container"
+    <div className="footer__container">
+      <div className="footer__contact"
       style={{
         display: "flex",
         justifyContent: "center",
-        
-        height: "300px",
-        width: "100%"
+        height: "200px",
+        width: "100%",
+        marginTop: "9rem"
       }}>
-      <div className="footer__contact">
         <div className="footer__contact_item">
         <a href="#footer"
         style={{
           textDecoration: "none",
-          color: "black"
-        }}><p className="">(021) 712 - 3390</p></a>
+          color: "black",
+          
+        }}><p className="footer__contact_number"
+          style={{
+            paddingRight: "2rem"
+          }}>(021) 712 - 3390</p></a>
         </div>
         <div className="footer__contact_item">
         <a href="#footer"
         style={{
           textDecoration: "none",
           color: "black"
-        }}><p className="">paintit@gmail.com</p></a>
+        }}><p className="footer__contact_email">paintit@gmail.com</p></a>
         </div>
       </div>
     </div>
